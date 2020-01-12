8aaa81435089e42248d9bc02c1563ea04f0b3567
cTu4 <chudik9898@mail.ru>
2020-01-12 17:23:57 +0300

    test

--- main.js
+++ main.js
@@ -2,26 +2,19 @@ Ext.onReady(function () {
     Ext.create('Ext.panel.Panel', {
         width: 500,
         height: 300,
+        margin: 50,
         title: 'Border Layout',
-        layout: 'border',
-        items: [
-            {
-            // xtype: 'panel' implied by default
-            title: 'West Region is collapsible',
-            region:'west',
-            xtype: 'panel',
-            margin: '5 0 0 5',
-            width: 200,
-            collapsible: true,   // make collapsible
-            id: 'west-region-container',
-            layout: 'fit'
-        },{
-            title: 'Center Region',
-            region: 'center',     // center region is required, no width/height specified
-            xtype: 'panel',
-            layout: 'fit',
-            margin: '5 5 0 0'
-        }],
+        items: {
+            xtype: 'button',
+            text: 'Vk auth',
+            margin: 50,
+            width: 100,
+            handler: function () {
+                var key = 'a9Mqx2Xf1KJ4K6RKTmP9';
+                var id ='7279137';
+            }
+
+        },
         renderTo: Ext.getBody()
     });
 });
