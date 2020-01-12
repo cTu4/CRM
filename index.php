e5e65f5db7454c7483318db75806023b63559de2
cTu4 <chudik9898@mail.ru>
2020-01-12 16:55:04 +0300

    start

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="ext-all-debug.js"></script>
+    <script type="text/javascript" src="main.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/theme-triton-all.css"/>
+    <title>CRM</title>
+</head>
+<body>
+
+</body>
+</html>
