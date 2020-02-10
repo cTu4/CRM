bbca3419da6ad287c8974a49bb64303e13ae91ca
cTu4 <chudik9898@mail.ru>
2020-02-10 11:51:22 +0300

    aaa

--- index.php
+++ index.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <script type="text/javascript" src="ext-all-debug.js"></script>
     <script type="text/javascript" src="main.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
     <link rel="stylesheet" type="text/css" href="css/theme-triton-all.css"/>
     <title>CRM</title>
 </head>
 <body>
-
+<div id="container"></div>
 </body>
 </html>
--- main.js
+++ main.js
@@ -1,4 +1,102 @@
 Ext.onReady(function () {
+    var categories = [2011,2012,2013,2014];
+    var series = [{
+        label: 'Гражданка',
+        name: 'Наука',
+        data: [-30, -10, -20, -12]
+    }, {
+        label: 'Гражданка',
+        name: 'Остальное',
+        data: [-10, -10, -15, -8]
+    }, {
+        label: 'Военка',
+        name: 'Наука',
+        data: [50, 50, 45, 50]
+    },
+        {
+            label: 'Военка',
+            name: 'Остальное',
+            data: [10, 30, 20, 30]
+        }];
+
+    Ext.create('Ext.panel.Panel', {
+        width: Ext.getBody().getWidth(),
+        height: 700,
+        title: 'Border Layout',
+        layout: 'border',
+
+        defaults: {
+            collapsible: true,
+            split: true,
+        },
+        items: [{
+            title: 'West Region is collapsible',
+            region:'west',
+            xtype: 'panel',
+            width: 200,
+        },{
+            title: 'Center Region',
+            region: 'center',
+            xtype: 'panel',
+            layout: 'fit',
+            collapsible: false,
+            html: '<div id="main"></div>'
+        }],
+        renderTo: Ext.getBody()
+    });
+
+    Highcharts.chart('main',{
+        chart: {
+            type: 'column'
+        },
+        width: '100%',
+        title: {
+            text: 'Динамика военной и гражданской промышленности'
+        },
+        xAxis: {
+            categories: categories
+        },
+        legend:{
+            enabled:false
+        },
+        yAxis:[
+            {
+                title: false,
+                reversed: false,
+                labels: {
+                    formatter: function () {
+                        return Math.abs(this.value) + '%';
+                    }
+                },
+
+
+            }
+        ],
+
+
+
+        tooltip: {
+           formatter: function(){
+               return '<b>'+this.point.series.userOptions.label+'</b><br/>'+this.series.name+':'+Math.abs(this.point.y)+'%<br/>Сумма:'+Math.abs(this.point.stackTotal)+'%';
+           },
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+            }
+        },
+        series: series
+    });
+
+
+
+
+
+
+
+
+
+    /*
     Ext.create('Ext.panel.Panel', {
         width: 500,
         height: 300,
@@ -18,5 +116,5 @@ Ext.onReady(function () {
 
         },
         renderTo: Ext.getBody()
-    });
+    });*/
 });
